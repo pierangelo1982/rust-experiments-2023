5c2aab6d3a6a07fe12d179307c35bca902b67ecd
pierangelo1982 <pierangelo1982@gmail.com>
2023-03-25 14:47:54 +0100

    create func

--- thumbnails_generator/src/main.rs
+++ thumbnails_generator/src/main.rs
@@ -1,9 +1,27 @@
 use image::GenericImageView;
+use std::fs;
+use std::path::Path;
 
 fn main() {
 
-    println!("Hello, world!");
-    let img = image::open("images/originals/image_1.jpg").unwrap();
+    println!("thumbnail generator");
+    // read files in folder
+    for file in fs::read_dir("images/originals").unwrap() {
+        let img_path: String = file.unwrap().path().display().to_string();
+        println!("show me the path: {}", img_path);
+        thumbnail_generator("images/originals/image_1.jpg");
+        //println!("{}", file.unwrap().path().display());
+    }
+   }
+
+
+fn thumbnail_generator(image_path: &str) {
+    println!("percordo immagine {:?}", image_path);
+     let img = image::open(image_path).unwrap();
+    
+     //let filename: str = image_path.;
+     let filename = Path::new(& image_path).file_name();
+     println!("File name is {:?}", filename);
     
     println!("dimensions {:?}", img.dimensions());
    
@@ -15,5 +33,12 @@ fn main() {
 
     //img.resize(300, 300, image::imageops::FilterType::Gaussian);
     let thumb  = img.thumbnail(width/10, height/10);
-    thumb.save("images/thumbnails/image_1.jpg").unwrap();
+
+    let filename_tmp: String = format!("{:?}", filename).to_string();
+    let mut save_thumbnail_path: String=  "images/thumbnails/".to_owned();
+    let borrowed_string: String = save_thumbnail_path + filename_tmp;
+    
+    thumb.save(borrowed_string).unwrap();
+
+
 }
