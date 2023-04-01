227156b7bfb13fbe6ac8f07b972fa0e2d153dfef
Pierangelo Orizio <pierangelo82@outlook.it>
2023-04-01 17:55:43 +0200

    add input entry

---
+++ thumbnails_generator_input/src/main.rs
@@ -0,0 +1,69 @@
+use image::GenericImageView;
+use std::fs;
+use std::io;
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+    println!("thumbnail generator");
+
+    let stdin = io::stdin();
+
+    let mut image_original_folder_path_input = String::new();
+    println!("inserisci il percorso della cartella immagini:");
+    stdin.read_line(&mut image_original_folder_path_input)?;
+
+    let mut image_thumbnails_folder_path_input = String::new();
+    println!("inserisci il percorso della cartella dove salvare le thumbnails:");
+    stdin.read_line(&mut image_thumbnails_folder_path_input)?;
+
+    println!(
+        "elaboro le immagini nel percorso: {} ",
+        image_original_folder_path_input
+    );
+    println!(
+        "salvo le thumbnails nel percorso: {} ",
+        image_thumbnails_folder_path_input
+    );
+
+    // read files in folder
+    for file in fs::read_dir(image_original_folder_path_input).unwrap() {
+        let img_path: String = file.unwrap().path().display().to_string();
+        println!("show me the path: {}", img_path);
+
+        thumbnail_generator(&img_path, &image_thumbnails_folder_path_input);
+    }
+
+    Ok(())
+}
+
+fn thumbnail_generator(image_path: &str, thumbnails_save_path: &str) {
+    println!("percordo immagine {:?}", image_path);
+    let img = image::open(image_path).unwrap();
+
+    let filename = Path::new(&image_path)
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    println!("File name is {:?}", filename);
+
+    println!("dimensions {:?}", img.dimensions());
+
+    let width: u32 = img.width();
+    let height: u32 = img.height();
+
+    println!("width: {:?}", width);
+    println!("height: {:?}", height);
+
+    if width > 1200 {
+        let thumb = img.thumbnail(1200, 1200);
+
+        let save_path: String = thumbnails_save_path.to_owned();
+        let new_path = save_path + filename;
+        println!("percorso salvataggio thumbnails: {}", new_path);
+
+        thumb.save(new_path).unwrap();
+    } else {
+        println!("inferiore")
+    }
+}
